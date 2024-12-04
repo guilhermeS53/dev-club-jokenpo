36f7c0e8554e414d75793acc211ba4ccbc4577db
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-12-03 21:29:25 -0300

    finalizacao da logica do projeto

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const result = document.querySelector(".result");
+const humanScore = document.querySelector("#human-score");
+const machineScore = document.querySelector("#machine-score");
+
+let humanScoreNumber = 0;
+let machineScoreNumber = 0;
 
 const playHuman = (humanChoice) => {
   playTheGame(humanChoice, playMachine());
@@ -21,8 +26,12 @@ const playTheGame = (human, machine) => {
     (human === "paper" && machine === "rock") ||
     (human === "scissors" && machine === "paper")
   ) {
+    humanScoreNumber++;
+    humanScore.innerHTML = humanScoreNumber;
     result.innerHTML = "Você VENCEU a máquina!";
   } else {
+    machineScoreNumber++;
+    machineScore.innerHTML = machineScoreNumber;
     result.innerHTML = "Você PERDEU para máquina!";
   }
 };
